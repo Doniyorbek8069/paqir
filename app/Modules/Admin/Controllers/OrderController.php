fbe6fbdc35f2b976177ba58ba5882ee229229104
Doniyorbek8069 <doniyorbek8069@gmail.com>
2025-04-10 12:27:34 +0500

    fix

--- app/Modules/Admin/Controllers/OrderController.php
+++ app/Modules/Admin/Controllers/OrderController.php
@@ -27,14 +27,18 @@ class OrderController extends Controller
         ]);
         $total = 0;
         foreach($request->products as $product){
-            OrderProduct::query()->create([
-                'order_id'      => $order->id,
-                'product_id'    => $product['id'],
-                'number'        => $product['amount'],
-                'price'         => $product['price'],
-                'cost_price'    => $product['cost_price']
-            ]);
-            $total = $total + $product['amount'] * $product['price'];
+            foreach($request->products as $product){
+                if(($product['amount'] > 0) && ($product['price'] > 0) && ($product['cost_price'] > 0)){
+                    OrderProduct::query()->create([
+                        'order_id'      => $order->id,
+                        'product_id'    => $product['id'],
+                        'number'        => $product['amount'],
+                        'price'         => $product['price'],
+                        'cost_price'    => $product['cost_price']
+                    ]);
+                    $total = $total + $product['amount'] * $product['price'];
+                }
+            }
         }
 
         Order::query()->where('id','=',$order->id)->update([
